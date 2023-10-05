9d51954b1f9c63842e8a50cfa9e27ded21e55ff4
GiovanyDeMurel-HoGent <giovany.demurel@student.hogent.be>
2023-10-05 20:22:21 +0200

    Initial commit

---
+++ Services/InvoicePdfGenerator.cs
@@ -0,0 +1,31 @@
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+
+namespace QuestPDF.ExampleInvoice
+{
+    public class GenerateInvoice
+    {
+    public static void GenerateInvoicePdf()
+        {
+            // TODO: Please make sure that you are eligible to use the Community license.
+            // To learn more about the QuestPDF licensing, please visit:
+            // https://www.questpdf.com/pricing.html
+            Settings.License = LicenseType.Community;
+            
+            // For documentation and implementation details, please visit:
+            // https://www.questpdf.com/documentation/getting-started.html
+            var model = InvoiceDocumentDataSource.GetInvoiceDetails();
+            var document = new InvoiceDocument(model);
+
+            // Generate PDF file and show it in the default viewer
+            document.GeneratePdf("example.pdf");
+            // document.GeneratePdfAndShow();
+            
+            // Or open the QuestPDF Previewer and experiment with the document's design
+            // in real-time without recompilation after each code change
+            // https://www.questpdf.com/document-previewer.html
+            //document.ShowInPreviewer();
+        }
+    }
+}
